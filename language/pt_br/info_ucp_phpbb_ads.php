1540822e9e76221872796361ff3abb0bdc0f5f70
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-12-25 10:44:41 -0200

    Atualizado

--- language/pt_br/info_ucp_phpbb_ads.php
+++ language/pt_br/info_ucp_phpbb_ads.php
@@ -2,7 +2,7 @@
 /**
  *
  * Advertisement management. An extension for the phpBB Forum Software package.
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.3] (https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.4] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2017 phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
@@ -19,6 +19,6 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'UCP_PHPBB_ADS_TITLE'	=> 'Anúncios',
+	'UCP_PHPBB_ADS_TITLE'	=> 'Meus anúncios',
 	'UCP_PHPBB_ADS_STATS'	=> 'Estatisticas',
 ));
--- language/pt_br/ucp.php
+++ language/pt_br/ucp.php
@@ -2,7 +2,7 @@
 /**
  *
  * Advertisement management. An extension for the phpBB Forum Software package.
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.3] (https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.4] (https://github.com/phpBBTraducoes)
  * @copyright (c) 2017 phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
@@ -19,8 +19,13 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'AD_NAME'	=> 'Nome',
-	'AD_VIEWS'	=> 'Visualizações',
-	'AD_CLICKS'	=> 'Cliques',
-	'NO_ADS'	=> '<strong>Nenhum anúncio para exibir. Os usuários que possuem anúncios exibidos neste fórum podem visualizar suas estatísticas aqui.</strong>',
+	'AD_NAME'	    => 'Nome',
+	'AD_END_DATE'	=> 'Data final',
+	'AD_VIEWS'	    => 'Visualizações',
+	'AD_CLICKS'	    => 'Cliques',
+	'AD_STATUS'		=> 'Status',
+	'EXPIRED'		=> 'Expirou',
+	'ACTIVE_ADS'	=> 'Anúncios ativos',
+	'EXPIRED_ADS'	=> 'Anúncios expirados',
+	'NO_ADS'	    => '<strong>Você não possui anúncios exibidos neste fórum.</strong>',
 ));
